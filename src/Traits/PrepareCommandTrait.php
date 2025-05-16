8151c32281761d8a8b2dd702be2db14be93ade1f
tms-quivo <qui.vo@tomosia.com>
2025-05-16 17:06:09 +0700

    feat: generator controller and request

---
+++ src/Traits/PrepareCommandTrait.php
@@ -0,0 +1,76 @@
+<?php
+namespace Tomosia\LaravelModuleGenerate\Traits;
+
+use Illuminate\Support\Str;
+
+trait PrepareCommandTrait
+{
+    /**
+     * Get the namespace for the given name.
+     *
+     * @return string
+     */
+    protected function getClassNamespace(): string
+    {
+        $path = config('module-generator.paths.' . strtolower($this->type));
+        $path = str($path)->append('\\' . $this->getSubNamespace())->chopEnd('\\')->toString();
+
+        return sprintf(
+            '%s\%s\%s',
+            trim($this->rootNamespace(), '\\'),
+            $this->option('module'),
+            $path
+        );
+    }
+
+    /**
+     * Get the sub namespace for the given name.
+     *
+     * @return string
+     */
+    protected function getSubNamespace(): string
+    {
+        if (! Str::contains($this->argument('name'), '\\')) {
+            return '';
+        }
+
+        return str_replace('\\' . $this->getClassName(), '', $this->argument('name'));
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub(): string
+    {
+        return __DIR__ . '/../Generators/Stubs/' . ucfirst($this->type) . 'Stub.stub';
+    }
+
+    /**
+     * Get the class name for the given name.
+     *
+     * @return string
+     */
+    protected function getClassName(): string
+    {
+        return class_basename($this->argument('name'));
+    }
+
+    protected function replaceStub(string $stub): string
+    {
+        $stub = $this->replaceGeneral($stub, $this->getSubNamespace());
+
+        return str_replace(
+            [
+                '{{ module }}',
+                '{{ name }}',
+            ],
+            [
+                $this->option('module'),
+                $this->getClassName(),
+            ],
+            $stub
+        );
+    }
+}

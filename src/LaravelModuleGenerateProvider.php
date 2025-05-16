cd07e0ab218ffb9f09551e98e0294d4b1e9ec46f
tms-quivo <qui.vo@tomosia.com>
2025-05-16 11:23:31 +0700

    first init

---
+++ src/LaravelModuleGenerateProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tomosia\LaravelModuleGenerate;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelModuleGenerateProvider extends ServiceProvider
+{
+	public function register()
+	{
+		
+	}
+
+    public function boot()
+    {
+        //
+    }
+}
+
